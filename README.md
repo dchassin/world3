b9bf47f150b736bd786413727e19b689a8742b08
David P. Chassin <david.chassin@me.com>
2026-06-27 09:18:18 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+World3 Model
+------------
+
+Based on Meadows [1] and Branderhorst [2].
+
+Example
+-------
+
+The following input to Python
+
+    import world3 as w3
+    test = w3.World3()
+    print(test)
+
+outputs
+
+                  population  resources   industry  agriculture  pollution
+    1900-01-01  1.600000e+09   1.000000  41.562500   269.325000   0.183824
+    1901-01-01  1.607237e+09   0.999717  42.314650   281.717904   0.107817
+    1902-01-01  1.617208e+09   0.999428  44.092965   278.147142   0.068004
+    1903-01-01  1.630342e+09   0.999125  45.588417   278.661263   0.047150
+    1904-01-01  1.644874e+09   0.998809  46.970601   279.837458   0.036446
+    ...                  ...        ...        ...          ...        ...
+    2096-01-01  4.095846e+09   0.151635  12.757071   231.631933   0.741359
+    2097-01-01  4.065166e+09   0.151413  12.144139   232.594775   0.703014
+    2098-01-01  4.034825e+09   0.151204  11.561135   233.606877   0.666518
+    2099-01-01  4.004811e+09   0.151005  11.006563   234.667747   0.631771
+    2100-01-01  3.975108e+09   0.150818  10.479016   235.776673   0.598684
+
+    [201 rows x 5 columns]
+
+
+References
+----------
+
+1. Meadows, Donella H., Randers, Jorgen and Meadows, Dennis L.. "The Limits to
+Growth (1972)". The Future of Nature: Documents of Global Change, edited by
+Libby Robin, Sverker Sörlin and Paul Warde, New Haven: Yale University Press,
+2013, pp. 101-116. https://doi.org/10.12987/9780300188479-012
+
+2. Branderhorst, Gaya. 2020. Update to Limits to Growth: Comparing the World3
+Model With Empirical Data. Master's thesis, Harvard Extension School.
---
+++ world3/world3.py
@@ -0,0 +1,107 @@
+"""World 3 model"""
+import datetime
+import pandas as pd
+import numpy as np
+import pyworld3 as w3
+
+class World3(pd.DataFrame):
+    """World 3 model implementation"""
+
+    SCALES = {
+            "population":9,
+            "resources":0,
+            "industry":2,
+            "agriculture":2,
+            "pollution":1,
+        }
+    def __init__(self,
+        year_min:int=1900,
+        year_max:int=2100,
+        dt:float=1,
+        pyear:int=1975,
+        verbose:bool=False,
+        **kwargs):
+        """Construct and solve the World3 model
+
+        Arguments
+        ---------
+        - `year_min`: starting year
+        - `year_max`: ending yeaer
+        - `dt`: timestep
+        - `pyear`: policy start year
+        - `verbose`: enable verbose output
+        - `scales`: scaling of data
+        - `kwargs`: model constants
+
+        Description
+        -----------
+        """
+
+        world3 = w3.World3(
+            year_min=year_min,
+            year_max=year_max,
+            dt=dt,
+            pyear=pyear,
+            verbose=verbose,
+            )                    # choose the time limits and step.
+        world3.init_world3_constants(**kwargs)       # choose the model constants.
+        world3.init_world3_variables()       # initialize all variables.
+        world3.set_world3_table_functions()  # get tables from a json file.
+        world3.set_world3_delay_functions()  # initialize delay functions.
+        world3.run_world3()
+        df = pd.DataFrame(
+            data={
+                "population": world3.pop,
+                "resources": world3.nrfr,
+                "industry": world3.iopc,
+                "agriculture": world3.fpc,
+                "pollution": world3.ppolx,
+            },
+            index=pd.DatetimeIndex([datetime.date(year=int(x),month=int((x-int(x))*12)+1,day=1) for x in world3.time])
+            )
+
+        super().__init__(df)
+
+    def scale(self,scales:float|dict=None):
+        if scales is None:
+            scales = self.SCALES
+
+        if isinstance(scales,(int,float)):
+            # print(f"{scales=}: {np.where(self.index.year==int(scales))[0]=}")
+            scales = f"{int(scales)}-01-01"
+            scales = {
+                "population":np.log10(self.population.loc[scales]),
+                "resources":np.log10(self.resources.loc[scales]),
+                "industry":np.log10(self.industry.loc[scales]),
+                "agriculture":np.log10(self.agriculture.loc[scales]),
+                "pollution":np.log10(self.pollution.loc[scales]),
+            }
+
+        df = pd.DataFrame(
+            data={
+                "population": self.population / np.pow(10,scales["population"]),
+                "resources": self.resources / np.pow(10,scales["resources"]),
+                "industry": self.industry / np.pow(10,scales["industry"]),
+                "agriculture": self.agriculture / np.pow(10,scales["agriculture"]),
+                "pollution": self.pollution / np.pow(10,scales["pollution"]),
+            },
+            index=self.index,
+            )
+        return df
+
+
+if __name__ == "__main__":
+
+    import matplotlib.pyplot as plt
+    
+    test = World3().scale()
+    print(World3.SCALES)
+    test.columns = [fr"{x.title()} ($\times 10^{y}$)" for x,y in zip(test.columns,World3.SCALES.values())]
+    print(test.columns,flush=True)
+    
+    test.plot(figsize=(10,7),grid=True,title="World 3 Model",xlabel="Year (CE)",ylabel="Value")
+
+    plt.show()
+
+
+
